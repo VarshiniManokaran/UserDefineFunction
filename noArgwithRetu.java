d1492fb38595c9dbcc17852d093e7f5d1b598003
VarshiniManokaran <varshini6678@gmail.com>
2024-08-20 15:25:31 +0530

    Add files via upload

---
+++ noArgwithRetu.java
@@ -0,0 +1,21 @@
+public class noArgwithRetu {
+    public static void main(String[] args) {
+      
+      System.out.println(sample.display());
+
+    }
+}
+
+class sample{
+ static  String display(){
+  String Animal="cat,rat,dog,elephant";
+  return Animal;
+
+
+  }
+}
+
+
+
+    
+    
