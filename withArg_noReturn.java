f6213a06e13edd62137b30b189a625f8fcd63e2f
VarshiniManokaran <varshini6678@gmail.com>
2024-08-20 16:31:44 +0530

    Add files via upload

---
+++ withArg_noReturn.java
@@ -0,0 +1,20 @@
+public class withArg_noReturn {
+
+    public static void habit(String Bark,String Rolling){
+    
+    }
+        static void Animal(int num,int num2){
+            //int c=num+num2;
+            System.out.println(num+num2);
+
+        }
+       
+
+    
+    public static void main(String[] args) {
+     habit("waw waw" , "uff uff");
+     Animal(21,54);
+
+    }
+    
+}
