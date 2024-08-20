6e3c9926776e0fbb012e32535cee38b91eda1d67
VarshiniManokaran <varshini6678@gmail.com>
2024-08-20 16:45:10 +0530

    Add files via upload

---
+++ withArgwithRet.java
@@ -0,0 +1,11 @@
+public class withArgwithRet {
+    static  int numbers(int a, int b){
+
+        return  a+b;
+    }
+    public static void main(String[] args) {
+         int f=numbers(127,110);
+         System.out.println(f);
+    }
+    
+}
