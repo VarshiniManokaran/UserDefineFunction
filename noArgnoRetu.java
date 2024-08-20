5650e925571588e6e7fc60ab10bd313dce65b724
VarshiniManokaran <varshini6678@gmail.com>
2024-08-20 15:30:56 +0530

    Add files via upload

---
+++ noArgnoRetu.java
@@ -0,0 +1,21 @@
+public class noArgnoRetu {
+    public static void main(String[] args) {
+        fooditems.IndianFood();
+        fooditems.chineseitems();
+          
+      }
+  }
+  class fooditems{
+  static void IndianFood(){
+      System.out.println("I like Indian Food");
+      System.out.println("No Arguments and No Returntype");
+  }
+      
+  
+    static void chineseitems(){
+  System.out.println("i like Chinese food");
+    }
+  }
+  
+    
+
